24d2a7b07b3078002fb37f34b103a90a89611c65
James <james.jao@hwtrek.com>
2015-09-01 17:27:57 +0800

    update redux and react-redux to 2.0.0

--- app/main.js
+++ app/main.js
@@ -2,17 +2,17 @@
 
 import 'babel-core/polyfill';
 import React from 'react';
+
 import { devTools, persistState } from 'redux-devtools';
 import AppWrap from './components/AppWrap';
 import { applyMiddleware, createStore, combineReducers,compose} from 'redux';
-
 import * as reducers from './reducers';
 
 //with redux dev-tools
 const finalCreateStore  = compose(
 	devTools(),
-	persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/)),
-	createStore);
+	persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/)))
+	(createStore);
 
 
 const composedReducers = combineReducers(reducers);
